06de7d492f433383effdf714842c3cfb7e4a7467
Danny <dannyxmcclain@gmail.com>
2018-09-05 15:26:14 -0500

    removed favicon and updated styles

--- src/App.js
+++ src/App.js
@@ -40,21 +40,7 @@ getTabsFromSessions = (sessions) => {
 }
 
 renderTabListItem = (tab) => {
-  return (
-    <li>
-      <a href={tab.url}>
-        <span className="favicon">
-        { (tab.favIconUrl !== '')
-          ?
-          <img src={tab.favIconUrl} alt="" width="100%" height="100%" />
-          :
-          <svg width="10" height="14" xmlns="http://www.w3.org/2000/svg"><g stroke="#5A5A5A" fill="none" fill-rule="evenodd"><path d="M.5.5h5l4 4v9h-9z"/><path d="M5.5.5v4h4"/></g></svg>
-          
-        }
-        </span>
-      {tab.title}</a>
-    </li>
-  )
+  return <li><a href={tab.url}>{tab.title}</a></li>
 }
 
   render() {
