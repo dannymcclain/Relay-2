77b57cbb39c8db43a287ec47211f691fd0f7d5cb
Danny <dannyxmcclain@gmail.com>
2018-09-06 15:15:16 -0500

    prettier-ed some files

--- src/App.js
+++ src/App.js
@@ -2,57 +2,70 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-/* eslint-disable no-undef */
+  /* eslint-disable no-undef */
 
-constructor(props) {
-  super(props);
-  this.state = {  
-    deviceTabs: []
-  };
-}
-
-componentDidMount() {
-  chrome.sessions.getDevices({}, this.createTabList);
-}
-
-createTabList = (data) => {
-  const tabsByDevice = this.getTabsByDevice(data);
-  this.setState ({
-    deviceTabs: tabsByDevice
-  }, console.log(tabsByDevice))
-}
-
-getTabsByDevice = (sessions) => {
-  return sessions.map((device) => {
-    const sessions = device.sessions;
-    const tabs = this.getTabsFromSessions(sessions);
-    return {
-      name: device.deviceName,
-      tabs,
+  constructor(props) {
+    super(props);
+    this.state = {
+      deviceTabs: []
     };
-  });
-}
+  }
 
-getTabsFromSessions = (sessions) => {
-  return sessions.reduce((acc, curr) => {
-    return [...acc, ...curr.window.tabs];
-  }, []);
-}
+  componentDidMount() {
+    chrome.sessions.getDevices({}, this.createTabList);
+  }
 
-renderTabListItem = (tab) => {
-  return <li><a href={tab.url} onClick={() => {chrome.tabs.create({url: tab.url})}}>{tab.title}<span className="tab-url">{tab.url}</span></a></li>
-}
+  createTabList = data => {
+    const tabsByDevice = this.getTabsByDevice(data);
+    this.setState(
+      {
+        deviceTabs: tabsByDevice
+      },
+      console.log(tabsByDevice)
+    );
+  };
+
+  getTabsByDevice = sessions => {
+    return sessions.map(device => {
+      const sessions = device.sessions;
+      const tabs = this.getTabsFromSessions(sessions);
+      return {
+        name: device.deviceName,
+        tabs
+      };
+    });
+  };
+
+  getTabsFromSessions = sessions => {
+    return sessions.reduce((acc, curr) => {
+      return [...acc, ...curr.window.tabs];
+    }, []);
+  };
+
+  renderTabListItem = tab => {
+    return (
+      <li>
+        <a
+          href={tab.url}
+          onClick={() => {
+            chrome.tabs.create({ url: tab.url });
+          }}
+        >
+          {tab.title}
+          <span className="tab-url">{tab.url}</span>
+        </a>
+      </li>
+    );
+  };
 
   render() {
     return (
       <div className="container">
-        {this.state.deviceTabs.map( (deviceTab)=> (
-        <div className="link-list">
-          <h2>{deviceTab.name}</h2>
-          <ul>
-            {deviceTab.tabs.map(this.renderTabListItem)}
-          </ul>
-        </div>
+        {this.state.deviceTabs.map(deviceTab => (
+          <div className="link-list">
+            <h2>{deviceTab.name}</h2>
+            <ul>{deviceTab.tabs.map(this.renderTabListItem)}</ul>
+          </div>
         ))}
       </div>
     );
